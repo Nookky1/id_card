ef857e2bb3fb633fbada7e7a0e81a50c4a346619
Nookky1 <worapong.nookky@gmail.com>
2021-12-29 09:40:41 +0700

    Method Post

--- app/models/users.py
+++ app/models/users.py
@@ -18,7 +18,7 @@ class ID(BaseModel):
     age : Optional[int] = Field(None, example=24)
     major : Optional[str] = Field(None,example='Computer Engineer')
     id_card : constr(min_length=13,max_length=13) = Field(example='1102002743832')
-    id : str = Field(None, example='ID ObjectID auto generate')
+    id : str = Field(example='ID ObjectID auto generate')
     
 class Users(BaseModel):
     Users : List[ID]
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException, status
 from typing import Optional
+
+from fastapi.params import Depends
+from pymongo import response
 from models.users import User,Users,ID
 from db.db import MongoDB
 from db import object_str
@@ -14,10 +17,27 @@ def generate_token(engine):
     Id = obj.dict()['id']
     return Id
 
+    
 @router.get('/')
 async def Get_users():
     users = db.find(collection='user',query={})
     users = list(users)
     return users
 
+
+@router.post('/')
+async def Create_User(payload:User):
+    payload = payload.dict()
+    payload['id'] = generate_token(engine=ObjectId())
+    card = db.find_one(collection='user',query={'id_card': payload['id_card']})
+    if card:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid user duplicate"
+        )
+    elif not card:
+        db.insert_one(collection='user',data=payload)
+        payload.pop('_id')
+        return payload
+    
     
